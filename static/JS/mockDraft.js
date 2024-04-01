f5040ea586a7f934d96b3bd7e2fe534236904e8f
c23037255 <HockleyB@cardiff.ac.uk>
2024-04-01 14:23:36 +0100

    designed draft year

---
+++ static/JS/mockDraft.js
@@ -0,0 +1,8 @@
+ActiveTeams = [] //Array for active teams
+
+for (i=0;i<32;i++){
+    document.getElementsByClassName("teamButton")[i].addEventListener("click",arrayActiveTeams)
+}
+function arrayActiveTeams{
+    console.log("ok" + i);
+}
