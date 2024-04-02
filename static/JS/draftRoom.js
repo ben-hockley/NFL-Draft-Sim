d3219058d7a162df51f77c14ccb42c3631ee97bf
c23037255 <HockleyB@cardiff.ac.uk>
2024-04-02 17:44:48 +0100

    adds teams to draft board using data from db

--- static/JS/draftRoom.js
+++ static/JS/draftRoom.js
@@ -3,14 +3,27 @@ draftOrder = document.getElementById('draftOrder').innerHTML;
 console.log(teamList);
 
 
-
-teamList = teamList.split(","); //turn into list of active teams
-
+// form arrays used to populate html file.
+teamList = teamList.split(",");
 draftOrder = draftOrder.substring(3,draftOrder.length-4);
-console.log(draftOrder);
-
 draftOrder = draftOrder.split("',), ('")
 
-console.log(teamList[0]);
-console.log(draftOrder)
-console.log(draftOrder.length)
+//write content to html file.
+for (i=0; i<draftOrder.length ; i++){
+    pick = document.createElement('div');
+    pick.className = 'pick';
+
+    pickNo = document.createElement('div');
+    pickNo.className = 'pickNo';
+    pickNo.innerHTML = i+1;
+
+    teamImage = document.createElement('img');
+    teamImage.setAttribute('src','/static/img/' + draftOrder[i] + '.webp')
+    teamImage.className = 'team';
+
+    pick.appendChild(pickNo);
+    pick.appendChild(teamImage);
+
+
+    document.getElementById('Picks').appendChild(pick);
+}
